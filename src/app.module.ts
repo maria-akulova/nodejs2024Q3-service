a603e679952a7fb47187403f4ff04606c0633528
maria.akulova <maria.akulova.dragon@gmail.com>
2024-11-11 19:24:00 +0100

    feat: auto generate entities, adjust package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FavoritesModule } from './favorites/favorites.module';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
+import { ArtistModule } from './artist/artist.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ArtistModule,
+    TrackModule,
+    AlbumModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
